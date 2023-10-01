c0fa9187d38c4994bd42e3a49218dfeda53a2b1f
minuyn <146628190+minuyn@users.noreply.github.com>
2023-10-01 20:20:08 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Khảo sát không gian nội thất", "Tư vấn các sản phẩm phù hợp","Thi công nội thất trong 24h","Hoàn thiện và bàn giao"],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
